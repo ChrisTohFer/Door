7e72c06d743591c8e625527e6197cfbcbfb0b5dd
Chris <chrisrkemp@hotmail.co.uk>
2018-09-01 18:55:12 +0100

    Introduced cardboard box mechanic

---
+++ Door/Assets/Scripts/BoxControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxControl : MonoBehaviour {
+
+    public Animator Controller;
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Controller.SetBool("SpaceDown", true);
+        }
+        else
+        {
+            Controller.SetBool("SpaceDown", false);
+        }
+    }
+}
