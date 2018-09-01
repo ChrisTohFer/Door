8b97734b078d006f33dbef9491aa1b60de4c2de5
Chris <chrisrkemp@hotmail.co.uk>
2018-09-01 10:04:17 +0100

    Implemented player movement

---
+++ Door/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float MaxSpeed;
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rb.velocity = MaxSpeed * new Vector3(x, 0f, z);
+
+        if (rb.velocity.magnitude > 0f)
+        {
+            transform.LookAt(transform.position + rb.velocity);
+            transform.rotation = transform.rotation * Quaternion.Euler(-90f, 90f, 0f);
+        }
+    }
+}
