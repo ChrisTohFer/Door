d0dea2b4300d927f80177d881e24c9a460fac884
Chris <chrisrkemp@hotmail.co.uk>
2018-09-01 19:15:45 +0100

    Misc

--- Door/Assets/Scripts/BoxControl.cs
+++ Door/Assets/Scripts/BoxControl.cs
@@ -6,15 +6,29 @@ public class BoxControl : MonoBehaviour {
 
     public Animator Controller;
 
+    public float BoxTime = 10f;
+    private float _timer;
+
+    private void Awake()
+    {
+        _timer = BoxTime;
+    }
+
     private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && _timer > 0f)
         {
             Controller.SetBool("SpaceDown", true);
+            _timer -= Time.fixedDeltaTime;
         }
         else
         {
             Controller.SetBool("SpaceDown", false);
         }
+    }
+
+    public void RefreshTime()
+    {
+        _timer = BoxTime;
     }
 }
