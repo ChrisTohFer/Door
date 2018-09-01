17f26d90adb6cfdcfbf89bd9f05a19ce857a7a87
Chris <chrisrkemp@hotmail.co.uk>
2018-09-01 15:00:02 +0100

    More art changes, added guards and detection

---
+++ Door/Assets/Scripts/GuardDetection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardDetection : MonoBehaviour {
+
+    public GameObject Exclamation;
+    public BoxCollider Col;
+    public float SearchLength = 5f;
+
+    private void OnEnable()
+    {
+        Col.size = new Vector3(SearchLength, 5f, SearchLength);
+        Col.transform.localPosition = new Vector3(0f, 0.1f, Mathf.Sqrt(0.5f * SearchLength * SearchLength));
+    }
+
+    private void PlayerCaught()
+    {
+        Debug.Log("Caught");
+        GameControl.PlayerCaught = true;
+        Exclamation.SetActive(true);
+    }
+    private void OnPlayerDetected(Collider other)
+    {
+        if ((other.transform.position - transform.parent.position).magnitude < SearchLength)
+        {
+            PlayerCaught();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !GameControl.PlayerCaught)
+        {
+            OnPlayerDetected(other);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !GameControl.PlayerCaught)
+        {
+            OnPlayerDetected(other);
+        }
+    }
+}
--- Door/Assets/Scripts/PlayerMovement.cs
+++ Door/Assets/Scripts/PlayerMovement.cs
@@ -14,12 +14,16 @@ public class PlayerMovement : MonoBehaviour {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        if (GameControl.PlayerCaught)
+        {
+            return;
+        }
         rb.velocity = MaxSpeed * new Vector3(x, 0f, z);
 
         if (rb.velocity.magnitude > MaxSpeed * 0.05f)
         {
             transform.LookAt(transform.position + rb.velocity);
-            transform.rotation = transform.rotation * Quaternion.Euler(-90f, 90f, 0f);
+            //transform.rotation = transform.rotation * Quaternion.Euler(-90f, 90f, 0f);
         }
     }
 }
