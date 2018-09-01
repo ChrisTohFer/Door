6279958d32c9c479befb449b8a9acafadd996fed
Dave Allen <daveallenbpm@gmail.com>
2018-09-01 16:37:18 +0100

    FMOD studio unity integration

---
+++ Door/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour {
+
+    [EventRef]
+    public string IntroSoundtrackRef;
+
+    [EventRef]
+    public string StealthSoundtrackRef;
+
+    private EventInstance IntroSoundtrack;
+    private EventInstance StealthSoundtrack;
+
+    // Use this for initialization
+    void Start() {
+        IntroSoundtrack = CreateFMODEventInstance(IntroSoundtrackRef);
+        StealthSoundtrack = CreateFMODEventInstance(StealthSoundtrackRef);
+
+        StealthSoundtrack.start();
+    }
+
+    private EventInstance CreateFMODEventInstance(string eventRef)
+    {
+        return RuntimeManager.CreateInstance(eventRef);
+    }
+}
