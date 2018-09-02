5b74294fca922d1ff18100f0f374f6f8dc6a8a73
Chris <chrisrkemp@hotmail.co.uk>
2018-09-02 10:31:19 +0100

    Added guard movement

---
+++ Door/Assets/Scripts/CircleGuard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleGuard : MonoBehaviour {
+
+    public float Speed;
+    public bool Clockwise;
+    public Rigidbody rb;
+    public Transform trans;
+
+    private bool _spunOut = false;
+    private float _radius, _circumferance;
+    private float _parameter;
+
+    private void OnEnable()
+    {
+        _radius = (trans.position - transform.position).magnitude;
+        _circumferance = 2f * Mathf.PI * _radius;
+        float angle = Vector3.SignedAngle(Vector3.forward, trans.position - transform.position, Vector3.up);
+        _parameter = Mathf.Repeat(angle * Mathf.PI / 180f, Mathf.PI * 2f);
+    }
+
+    private void FixedUpdate()
+    {
+        if(!_spunOut)
+        {
+            float way = Clockwise ? 1f : -1f;
+
+            _parameter += way * 2f * Mathf.PI * Speed * Time.deltaTime / _circumferance;
+            float x = transform.position.x + _radius * Mathf.Sin(_parameter);
+            float z = transform.position.z + _radius * Mathf.Cos(_parameter);
+            Vector3 direction = new Vector3(x, trans.position.y, z) - trans.position;
+            rb.velocity = direction.normalized * Speed;
+
+            trans.LookAt(direction + trans.position);
+        }
+    }
+
+    public void SpinOut()
+    {
+        _spunOut = true;
+    }
+}
---
+++ Door/Assets/Scripts/TurningGuard.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningGuard : MonoBehaviour
+{
+    [System.Serializable]
+    public class Turn
+    {
+        public float TargetAngle;
+        public float TurnSpeed;
+        public float PreWait;
+        public bool Clockwise;
+    }
+
+    public Rigidbody rb;
+    public Transform trans;
+    public int CurrentTurn;
+    public Turn[] TurnList;
+    
+    private float _timer;
+    private bool _spunOut = false;
+
+    private void OnEnable()
+    {
+        _timer = TurnList[CurrentTurn].PreWait;
+    }
+    private void FixedUpdate()
+    {
+        if (!_spunOut)
+        {
+            if (_timer > 0f)
+            {
+                _timer -= Time.fixedDeltaTime;
+            }
+            else
+            {
+                Turn current = TurnList[CurrentTurn];
+                float angle = AngularDifference();
+                float direction;
+                if (current.Clockwise)
+                {
+                    direction = 1f;
+                }
+                else
+                {
+                    direction = -1f;
+                }
+
+                if (angle < current.TurnSpeed * Time.fixedDeltaTime)
+                {
+                    trans.eulerAngles = new Vector3(0f, current.TargetAngle, 0f);
+                    rb.angularVelocity = Vector3.zero;
+                    Increment();
+                    _timer = TurnList[CurrentTurn].PreWait;
+                }
+                else
+                {
+                    rb.angularVelocity = new Vector3(0f, current.TurnSpeed * direction, 0f);
+                }
+            }
+        }
+    }
+
+    private void Increment()
+    {
+        CurrentTurn++;
+        if(CurrentTurn == TurnList.Length)
+        {
+            CurrentTurn = 0;
+        }
+    }
+    private float AngularDifference()
+    {
+        Turn current = TurnList[CurrentTurn];
+        float angle;
+        if(current.Clockwise)
+        {
+            if(trans.rotation.eulerAngles.y > current.TargetAngle)
+            {
+                angle = current.TargetAngle - trans.rotation.eulerAngles.y + 360f;
+            }
+            else
+            {
+                angle = current.TargetAngle - trans.rotation.eulerAngles.y;
+            }
+        }
+        else
+        {
+            if (trans.rotation.eulerAngles.y < current.TargetAngle)
+            {
+                angle = current.TargetAngle - trans.rotation.eulerAngles.y + 360f;
+            }
+            else
+            {
+                angle = current.TargetAngle - trans.rotation.eulerAngles.y;
+            }
+            angle *= -1f;
+        }
+        angle *= Mathf.PI / 180f;
+        return angle;
+    }
+    public void SpinOut()
+    {
+        _spunOut = true;
+    }
+}
